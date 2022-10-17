69d564cd542a5808b9124362532d0daf39a4b777
gyupetosz <gyupetosz@gmail.com>
2022-10-17 16:06:46 +0200

    Created the calculating class to get the distance between two points

--- Application/Calculations.cs
+++ Application/Calculations.cs
@@ -9,9 +9,20 @@ namespace Application
 {
     internal static class Calculations
     {
+        private static double ToRadians(double degrees)
+        {
+            return (degrees *= Math.PI / 180);
+        }
         public static double Distance(Position p1, Position p2)
         {
-            return 1;
+            double deltaLat = p1.Latitude > p2.Latitude ? p1.Latitude - p2.Latitude : p2.Latitude - p1.Latitude;
+            double deltaLong = p1.Longitude > p2.Longitude ? p1.Longitude - p2.Longitude : p2.Longitude - p1.Longitude;
+            deltaLat = ToRadians(deltaLat);
+            deltaLong = ToRadians(deltaLong);
+            double dist = Math.Pow(Math.Sin(deltaLat / 2), 2) + Math.Cos(ToRadians(p1.Latitude)) * Math.Cos(ToRadians(p2.Latitude)) * Math.Pow(Math.Sin(deltaLong / 2), 2);
+            dist = 2 * Math.Asin(Math.Sqrt(dist));
+            dist *= 6378137;
+            return dist;
         }
     }
 }
